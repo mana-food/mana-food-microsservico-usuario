b848614238acb22696a123e8e7aabb19923304b3
MarcelleTabosa <marcelle.tabosa.97@gmail.com>
2026-01-08 18:59:35 -0300

    refactor: removendo auth

--- Core/ManaFood.Application/Interfaces/Services/ITokenBlacklistService.cs
+++
@@ -1,8 +0,0 @@
-﻿// Esta interface permite extensão para diferentes estratégias de blacklist.
-namespace ManaFood.Application.Interfaces.Services;
-
-public interface ITokenBlacklistService
-{
-    void Add(string token);
-    bool IsBlacklisted(string token);
-}
